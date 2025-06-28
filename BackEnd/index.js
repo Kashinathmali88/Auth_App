9aab15e53e56cb1ba755ef1a3082c1bd4a46958c
kashinath mali <kashinathmali88@gmail.com>
2025-06-28 14:40:34 +0530

    added the new  backend url in cors

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -14,9 +14,9 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: [
+      "https://auth-app-frontend-25t0oiepy-kashinaths-projects-af06b8cd.vercel.app",
       "https://auth-app-backend-seven.vercel.app",
       "http://localhost:5173",
-      "https://auth-app-frontend-woad.vercel.app",
     ],
     credentials: true,
     exposedHeaders: ["set-cookie"],

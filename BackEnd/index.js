272c5030ef45a5b77ad43456dc4ae4fbd0d1a154
kashinath mali <kashinathmali88@gmail.com>
2025-06-29 15:18:20 +0530

    changed origin in backend

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -13,8 +13,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.ORIGIN, "http://localhost:5173"],
+    origin:
+      process.env.NODE_ENV === "production"
+        ? [process.env.ORIGIN]
+        : ["http://localhost:5173"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     exposedHeaders: ["set-cookie"],
   })
 );
